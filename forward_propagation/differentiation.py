096155b2cd0b28023e15da91df1dd00e27f82464
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-05-27 12:34:22 +0100

    __init__

--- forward_propagation/differentiation.py
+++ forward_propagation/differentiation.py
@@ -1,1 +1,10 @@
 """A first attempt at implementing forward-propagation differentiation."""
+
+
+class Dfloat:
+    """Implement forward-propagation differentiation."""
+
+    def __init__(self, x, dx):
+        """Initialise Dfloat."""
+        self.x = x
+        self.dx = dx
