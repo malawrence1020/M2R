533577d2094a098cb44f150df09fbb9cc38d4268
malawrence1020 <Matthew.X.Lawrence@protonmail.ch>
2021-05-28 13:26:42 +0100

    More special methods

--- forward_propagation/differentiation.py
+++ forward_propagation/differentiation.py
@@ -11,5 +11,35 @@ class Dfloat:
         self.dx = dx
 
     def __add__(self, other):
-        """Implement addition"""
+        """Implement addition."""
         return type(self)(self.x + other.x, self.dx + other.dx)
+
+    def __radd__(self, other):
+        """Reverse addition."""
+        return self + other
+
+    def __sub__(self, other):
+        """Implement subtraction."""
+        return type(self)(self.x - other.x, self.dx - other.dx)
+
+    def __rsub__(self, other):
+        """Reverse subtraction."""
+        return self - other
+
+    def __mul__(self, other):
+        """Implement multiplication."""
+        return type(self)(self.x * other.x,
+                          other.x * self.dx + self.x * other.dx)
+
+    def __rmul__(self, other):
+        """Reverse multiplication."""
+        return self * other
+
+    def __div__(self, other):
+        """Implement division."""
+        return type(self)(self.x / other.x, (other.x * self.dx +
+                          self.x * other.dx)/(other.x ** 2))
+
+    def __rdiv__(self, other):
+        """Reverse division."""
+        return self / other
