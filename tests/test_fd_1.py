74b201be0df400aaae31c1f49930d7defe7579f0
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-05-31 21:58:54 +0100

    Fixing some flake8 errors now that flake8 works

--- tests/test_fd_1.py
+++ tests/test_fd_1.py
@@ -1,12 +1,12 @@
 from forward_propagation import (Dfloat, sin, cos, tan, exp, log, sinh, cosh,
-                              tanh, asin, acos, atan, asinh, acosh, atanh)
+                                 tanh, asin, acos, atan, asinh, acosh, atanh)
 import pytest
 from numpy import allclose
 
-x1 = Dfloat(2,1)
-x2 = Dfloat(3,1)
-x3 = Dfloat(3.5,1)
-x4 = Dfloat(2,0.5)
+x1 = Dfloat(2, 1)
+x2 = Dfloat(3, 1)
+x3 = Dfloat(3.5, 1)
+x4 = Dfloat(2, 0.5)
 
 
 def test_type_error():
@@ -35,7 +35,7 @@ def test_add(f1, y1):
         (x1 - 1, Dfloat(1, 1)),
         (1 - x2, Dfloat(-2, -1)),
         (x3 - 1 - x4, Dfloat(0.5, 0.5)),
-        (x1 - 1 + x2 - x4 + 2 , Dfloat(4, 1.5))
+        (x1 - 1 + x2 - x4 + 2, Dfloat(4, 1.5))
     )
 )
 def test_sub(f2, y2):
@@ -47,7 +47,7 @@ def test_sub(f2, y2):
         (x1 * 2, Dfloat(4, 2)),
         (3 * x2, Dfloat(9, 3)),
         (x3 * 2.5 * x4, Dfloat(17.5, 9.375)),
-        ((x1 + 1) * x4 * (2 - x2) , Dfloat(-6, -9.5))
+        ((x1 + 1) * x4 * (2 - x2), Dfloat(-6, -9.5))
     )
 )
 def test_mul(f3, y3):
