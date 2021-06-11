9acc988b5384d4bbe45afb358e394348056e0123
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-06-11 08:15:28 +0100

    Methods

--- back_propagation/adjoint.py
+++ back_propagation/adjoint.py
@@ -1,4 +1,5 @@
 """A first attempt at implementing adjoint algorithmic differentiation."""
+import math
 from functools import wraps
 from numbers import Number
 
@@ -104,6 +105,118 @@ class AdjFloat:
     def __rpow__(self, other):
         """Reverse exponentiation."""
         return other ** self
+
+
+@dealing_with_other_types
+def sin(x):
+    """Implement sin for AdjFloat."""
+    result = type(x)(math.sin(x.val), 0)
+    tape.append(SinBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def cos(x):
+    """Implement cos for AdjFloat."""
+    result = type(x)(math.cos(x.val), 0)
+    tape.append(CosBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def tan(x):
+    """Implement tan for AdjFloat."""
+    result = type(x)(math.tan(x.val), 0)
+    tape.append(TanBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def exp(x):
+    """Implement exp for AdjFloat."""
+    result = type(x)(math.exp(x.val), 0)
+    tape.append(ExpBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def log(x):
+    """Implement log for AdjFloat."""
+    result = type(x)(math.log(x.val), 0)
+    tape.append(LogBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def sinh(x):
+    """Implement sinh for AdjFloat."""
+    result = type(x)(math.sinh(x.val), 0)
+    tape.append(SinhBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def cosh(x):
+    """Implement cosh for AdjFloat."""
+    result = type(x)(math.cosh(x.val), 0)
+    tape.append(CoshBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def tanh(x):
+    """Implement tanh for AdjFloat."""
+    result = type(x)(math.tanh(x.val), 0)
+    tape.append(TanhBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def asin(x):
+    """Implement asin for AdjFloat."""
+    result = type(x)(math.asin(x.val), 0)
+    tape.append(AsinBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def acos(x):
+    """Implement acos for AdjFloat."""
+    result = type(x)(math.acos(x.val), 0)
+    tape.append(AcosBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def atan(x):
+    """Implement atan for AdjFloat."""
+    result = type(x)(math.atan(x.val), 0)
+    tape.append(AtanBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def asinh(x):
+    """Implement asinh for AdjFloat."""
+    result = type(x)(math.asinh(x.val), 0)
+    tape.append(AsinhBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def acosh(x):
+    """Implement acosh for AdjFloat."""
+    result = type(x)(math.acosh(x.val), 0)
+    tape.append(AcoshBlock(result, x))
+    return result
+
+
+@dealing_with_other_types
+def atanh(x):
+    """Implement atanh for AdjFloat."""
+    result = type(x)(math.atanh(x.val), 0)
+    tape.append(AtanhBlock(result, x))
+    return result
 
 
 class Block:
--- tests/test_adj.py
+++ tests/test_adj.py
@@ -14,7 +14,7 @@ x5 = AdjFloat(3, 2)
     "f1, y1", (
         (x1.derivative(x1), 1),
         ((x1 + x1).derivative(x1), 2),
-        ((x1 + x2 + x3 + x4 + x5).derivative(x1, x2, x3, x4, x5), (1, 1, 1, 1, 1))
+        ((x1 + x2 + x3 + x4).derivative(x1, x2, x3, x4, x5), (1, 1, 1, 1, 0))
     )
 )
 def test_add(f1, y1):
