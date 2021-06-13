070370e68d7fbcf4cff41f8bf95ba0d330a0f2aa
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-06-13 18:39:12 +0100

    That's all folks!

--- tests/test_adj.py
+++ tests/test_adj.py
@@ -1,14 +1,7 @@
 """Pytests for AdjFloat and other adjoint methods."""
 from back_propagation import (AdjFloat, sin, cos, tan, exp, log, # noqa F401
                               sinh, cosh, tanh, asin, acos, atan, # noqa F401
-                              asinh, acosh, atanh, AddBlock, # noqa F401
-                              SubBlock, MulBlock, DivBlock, # noqa F401
-                              PowBlock, SinBlock, CosBlock, # noqa F401
-                              TanBlock, ExpBlock, LogBlock, # noqa F401
-                              SinhBlock, CoshBlock, TanhBlock, # noqa F401
-                              AsinBlock, AcosBlock, AtanBlock, # noqa F401
-                              AsinhBlock, AcoshBlock, AtanhBlock, # noqa F401
-                              clear_tape) # noqa F401
+                              asinh, acosh, atanh, clear_tape) # noqa F401
 import pytest
 import math
 from numpy import allclose
